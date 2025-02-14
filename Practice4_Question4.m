5b128d19ce680451702065036f09bf3bf5bc9517
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2025-02-14 17:54:58 +0600

    Create Practice4_Question4.m

---
+++ Practice4_Question4.m
@@ -0,0 +1,17 @@
+% Done by Sumaiya
+
+A = [1, 2, 4, 4];  
+M = 4;             
+
+F = zeros(1, M);   
+
+for u = 0:M-1
+    sum = 0;  
+    for i = 0:M-1
+        sum = sum + A(i+1) * exp(-1j * 2 * pi * u * i / M);
+    end
+    F(u+1) = sum;
+end
+
+disp('Fourier Spectrum:');
+disp(F);
